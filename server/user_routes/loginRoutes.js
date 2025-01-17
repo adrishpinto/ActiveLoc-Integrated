0ae592a2f98ed42afd59f6cc4ebfa1b26aa86a91
Joshua <adrishpinto423@gmail.com>
2025-01-17 17:11:09 +0530

    res cookie additional settings

--- server/user_routes/loginRoutes.js
+++ server/user_routes/loginRoutes.js
@@ -34,7 +34,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
       maxAge: 3600000,
     });
 
